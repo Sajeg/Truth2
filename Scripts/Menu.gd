4f720ec54854ab0ffacfc86943cc37790fbb6ce6
Sajeg <jfx@posteo.de>
2022-11-17 15:17:20 +0100

    changed icons(again)

---
+++ Scripts/Menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+
+func _on_TouchScreenButton_pressed():
+	$transition/AnimationPlayer.play("fade_right_start")
+	yield(get_tree().create_timer(0.6),"timeout")
+	if get_tree().change_scene("res://Scenes/Main.tscn") == OK:
+		pass
+	else:
+		print("Error changing Scene")
