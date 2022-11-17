d320e7afda21a891ac46ede28830b652c818a22f
Sajeg <jfx@posteo.de>
2022-11-17 20:56:49 +0100

    Added menu entries

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -11,3 +11,25 @@ func _on_TouchScreenButton_pressed():
 		pass
 	else:
 		print("Error changing Scene")
+
+func _on_ExitButton_pressed():
+	get_tree().quit()
+
+
+func _on_CreditsButton_pressed():
+	$transition/AnimationPlayer.play("fade_right_start")
+	yield(get_tree().create_timer(0.6),"timeout")
+
+	$Name.text = "Credits"
+
+	$VBoxContainer/Import.visible = false
+	$VBoxContainer/Credits.visible = false
+	$VBoxContainer/Exit.visible = false
+
+	$VBoxContainer/Credits1.visible = true
+	$VBoxContainer/Credits2.visible = true
+	$VBoxContainer/Credits3.visible = true
+	$VBoxContainer/Credits4.visible = true
+
+	
+	$transition/AnimationPlayer.play("fade_left_end")
